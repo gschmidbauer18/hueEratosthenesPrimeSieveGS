2841e92b20aa96f878c1f96c810044523c792ede
gschmidbauer18 <Schmidbauer.Gabriel@sus.htl-grieskirchen.at>
2020-09-21 15:15:28 +0200

    initial commit

---
+++ EratosthenesPrimeSieveGS/src/EratosthenesPrimeSieve/PrimeSieve.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EratosthenesPrimeSieve;
+
+/**
+ *
+ * @author 43676
+ */
+public interface PrimeSieve {
+    public boolean isPrime(int p);
+    public void printPrimes();
+}
