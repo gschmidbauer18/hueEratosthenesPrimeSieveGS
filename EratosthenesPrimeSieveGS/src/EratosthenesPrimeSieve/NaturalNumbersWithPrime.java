c564f020c00faf6d536d8d30a68acbcfa708830e
gschmidbauer18 <Schmidbauer.Gabriel@sus.htl-grieskirchen.at>
2020-09-25 16:18:29 +0200

    New Class for Task 2

---
+++ EratosthenesPrimeSieveGS/src/EratosthenesPrimeSieve/NaturalNumbersWithPrime.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EratosthenesPrimeSieve;
+
+/**
+ *
+ * @author 43676
+ */
+public class NaturalNumbersWithPrime {
+    
+}
