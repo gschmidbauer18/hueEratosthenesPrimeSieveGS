c6d9163ba226cb6924d6249f02d1d709b19546e8
gschmidbauer18 <Schmidbauer.Gabriel@sus.htl-grieskirchen.at>
2020-09-25 16:21:31 +0200

    List with even Numbers

--- EratosthenesPrimeSieveGS/src/EratosthenesPrimeSieve/NaturalNumbersWithPrime.java
+++ EratosthenesPrimeSieveGS/src/EratosthenesPrimeSieve/NaturalNumbersWithPrime.java
@@ -5,10 +5,34 @@
  */
 package EratosthenesPrimeSieve;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author 43676
  */
 public class NaturalNumbersWithPrime {
+
+    int obergrenze;
+    
+    public NaturalNumbersWithPrime(int pobergrenze) {
+        obergrenze=pobergrenze;
+    }
+    
+    public void numbersWithPrime()
+    {
+        ArrayList<Integer> evenNumbers=new ArrayList<Integer>();
+        
+        for (int i = 3; i <= obergrenze; i++) {
+            if(i%2==0)
+            {
+                evenNumbers.add(i);
+            }
+        }
+        
+        for (int i = 0; i < evenNumbers.size(); i++) {
+            System.out.println(evenNumbers.get(i)+" summe: "+" = "+" + ");
+        }
+    }
     
 }
